9156e4d024c7c9da58c7574880781f4fca06df5c
Krittanan Pingclasai <kp@kpping.me>
2017-10-06 17:51:57 +0700

    fix bug port

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ DATA="$HOME/Docker/$NAME/data"
 
 mkdir -p $DATA
 
-docker run --name $NAME -p 3306:$PORT -e MYSQL_ROOT_PASSWORD=$PASSWORD -e MYSQL_ROOT_HOST=172.17.0.1 -d -v $DATA:/var/lib/mysql mysql/mysql-server:$TAG
+docker run --name $NAME -p $PORT:3306 -e MYSQL_ROOT_PASSWORD=$PASSWORD -e MYSQL_ROOT_HOST=172.17.0.1 -d -v $DATA:/var/lib/mysql mysql/mysql-server:$TAG
